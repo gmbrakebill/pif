3568f135604c6ef986309f9a6fd486722d945e1e
gmbrakebill <64878965+gmbrakebill@users.noreply.github.com>
2020-07-10 08:26:53 -0500

    update

---
+++ front-end/build/precache-manifest.64db27aec3ce0b700af0ada734b13d86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d70892212326fdc8a5e2ad30d59bcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfa0dc9a8fb7b4ddc2d7",
+    "url": "/static/css/main.85e4be86.chunk.css"
+  },
+  {
+    "revision": "21d54e4a6a996e6ef560",
+    "url": "/static/js/2.8a4d08a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8a4d08a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa0dc9a8fb7b4ddc2d7",
+    "url": "/static/js/main.62ed9135.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
---
+++ front-end/build/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000;
+
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+}
+app.use(allowCrossDomain);
+
+console.log(__dirname);
+app.use('/', express.static(path.join(__dirname, '.')));
+app.use('/', express.static(path.join(__dirname, '/static')));
+app.use(express.static(path.join(__dirname, '/static/css')));
+app.use(express.static(path.join(__dirname, '/static/js')));
+
+app.listen(port, () => {
+    console.log(`epr_tracker frontend running on port ${port}.`)
+  })
